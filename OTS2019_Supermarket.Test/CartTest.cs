f50cb1dea4f2f086b246754173b985e5ef00be33
StudentM <Student@DESKTOP-B6DA8O4>
2022-03-30 16:00:18 +0200

    Vezbe 4

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -3,10 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OTS_Supermarket.Models;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
+        [Test]
+        public void AddOneToCart_OKInput_Success()
+        {
+            //1. Pravimo objekte za nase elemente 
+            Cart cart = new Cart();
+            Keyboard keyboard = new Keyboard();
+            //Act
+            cart.AddOneToCart(keyboard);
+            //Assert
+            Assert.AreEqual(1, cart.size);
+
+        }
+        //3. Testiramo popunjenost, tj uslov zdt od 10 itema..
+        [Test]
+        public void AddOneToCart_SizeGreaterThan10_Success()
+        {
+            Cart cart = new Cart();
+
+            Keyboard keyboard1 = new Keyboard();
+            Keyboard keyboard2 = new Keyboard();
+            Keyboard keyboard3 = new Keyboard();
+            Keyboard keyboard4 = new Keyboard();
+            Keyboard keyboard5 = new Keyboard();
+            Keyboard keyboard6 = new Keyboard();
+            Keyboard keyboard7 = new Keyboard();
+            Keyboard keyboard8 = new Keyboard();
+            Keyboard keyboard9 = new Keyboard();
+            Keyboard keyboard10 = new Keyboard();
+
+            cart.AddOneToCart(keyboard1);
+            cart.AddOneToCart(keyboard2);
+            cart.AddOneToCart(keyboard3);
+            cart.AddOneToCart(keyboard4);
+            cart.AddOneToCart(keyboard5);
+            cart.AddOneToCart(keyboard6);
+            cart.AddOneToCart(keyboard7);
+            cart.AddOneToCart(keyboard8);
+            cart.AddOneToCart(keyboard9);
+            cart.AddOneToCart(keyboard10);
+
+            //Testiramo 10. ekstra item, ocekujemo gresku
+            //Ali nemamo poruku koju program izbacuje, moramo ju napisati
+            Monitor monitor = new Monitor();
+            //cart.AddOneToCart(monitor); <- ide u poruku, ovo je metoda koja inicra poruku.
+            //<exception> hvatamo SVE exceptione
+            Exception ex = Assert.Throws<Exception>(() => cart.AddOneToCart(monitor));
+            //Proveri da li se ispisala bas excepcija koju mi zelimo..
+            Assert.That(ex.Message, Is.EqualTo("Number of items in cart must be 10 or less!"));
+        }
+
+        //AddMultiple, pitamo se prvo sta je input, sta je output
+        //Za domaci deleteAll, Print, Calculate; Brisanje , printovanje , 1 2 slucaja, kalkulacije imacemo vise slucajeva zvog restrikcija
+        [Test]
+        public void AddMultipleToCart_OKInput_Success()
+        {
+            //Objekti
+            Cart cart = new Cart();
+            Keyboard keyboard = new Keyboard();
+         
+            List<Item> keyboards = new List<Item>();
+
+            keyboards.Add(keyboard);
+            keyboards.Add(keyboard);
+            keyboards.Add(keyboard);
+            cart.AddMultipleToCart(keyboards);
+
+            Assert.AreEqual(3, cart.size);
+        }
+
+        [Test]
+        public void AddMultipleToCart_GreaterThan10 //VISE SLUCAJEVA??
+        {
+            //
+        }
     }
 }
--- OTS2019_Supermarket/Cart.cs
+++ OTS2019_Supermarket/Cart.cs
@@ -9,6 +9,49 @@ namespace OTS_Supermarket
 {
     public class Cart
     {
-        
+        //AddOneToCart
+        //input: neki item
+        //output: none
+
+        //2. Sada smo se vratili iz test klase i resavamo problem Kart i liste..
+
+        public List<Item> items;
+        public int size;
+        public double amount;
+
+        public Cart()
+        {
+            items = new List<Item>();
+            amount = 0;
+            size = 0;
+        }
+        //Nakon konstruktora , krece prazna metoda.. Krenemo test , ne radi.. 
+        //Resavamo dodavanjem size i itema.. Test OK
+        public void AddOneToCart(Item item)
+        {
+            if(size >= 10)
+            {
+                throw new Exception("Number of items in cart must be 10 or less!");
+            }
+            items.Add(item);
+            size++;
+            amount += item.Price;
+            //Pokrenuli smo test SizeGreaterThan10 , Failed..
+            //resavamo sada tako sto napisemo if sa porukom (pre svega)
+            //Fali nam isto i cena korpe , moramo i to kreirati kao parametar , refaktorisemo!
+            
+            //Mozemo prositi test metode sa proverom cene proizvoda ,
+            //ako zelimo , ili prosledjenje praznog itema da li radi metoda
+        }
+        public void AddMultipleToCart(List<Item> itemsList)
+        {
+            if(size >= 10)
+            {
+                throw new Exception("Number of items in cart must be 10 or less!");
+            }
+            items.AddRange(itemsList);
+            size += itemsList.Count;
+            //amount += itemsList.Count * itemsList??.Price;
+        }
     }
 }
